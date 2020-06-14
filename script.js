31c32b777425653a7cf9babfd10821063205def8
My-Hoa-Tien <myhoatien@gmail.com>
2020-06-14 21:40:19 +0100

    calculation display merged with input display function

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ let btnArray = [...calculatorBtns];
 
 // creating eventlisteners to all buttons
 for (let i = 0; i < btnArray.length; i++) {
-    btnArray[i].addEventListener("click", showWhatIsPressed);
-    btnArray[i].addEventListener("click", showCalculation);
+    btnArray[i].addEventListener("click", showInput);
+    btnArray[i].addEventListener("click", showInput);
 }
 
 // target the pressing button display
@@ -15,15 +15,23 @@ const pressedBtnDisplay = document.getElementById("pressed-btn");
 // variable for pressed button display
 let btnDisplay = "";
 
+// target for calculation display container
+const calculationDisplay = document.getElementById("calculation");
+
+// variable for showing calculation
+let calcDisplay = "";
+
 const symbolArray = ["+", "-", "x", "/"];
 
-function showWhatIsPressed(event) {
+function showInput(event) {
+    
     // variable for the button pressed
     let target = event.target.innerText;
 
     // if statement for clear button pressed
     if (target === "AC") {
-        btnDisplay = "";
+        btnDisplay = "0";
+        calcDisplay = "";
         totalSum = 0;
         totalArray = [];
     }
@@ -34,32 +42,39 @@ function showWhatIsPressed(event) {
         if (previousTarget === "=") {
             totalArray.push(target);
             btnDisplay = target;
+            calcDisplay = totalSum + target;
             totalSum = 0;
         } else if (symbolArray.includes(previousTarget)) {
             totalArray.splice(totalArray.length - 1, 1);
             btnDisplay = target;
+            calcDisplay = calcDisplay.substr(0,calcDisplay.length-1) + target;
             totalArray.push(btnDisplay)
         } else {
             totalArray.push(btnDisplay);
             btnDisplay = target;
+            calcDisplay = calcDisplay + target;
             totalArray.push(btnDisplay)
         }
     }
     // if statement for when decimal is pressed and the number doesnt have a decimal already
     if (target === "." && pressedBtnDisplay.innerText.includes(".") === false) {
         btnDisplay = btnDisplay + target;
+        calcDisplay = calcDisplay + target;
     }
 
     // if statement for when numbers are pressed
     if (target <= 9 && target >= 0) {
         if (symbolArray.includes(btnDisplay)) {
             btnDisplay = target;
+            calcDisplay = calcDisplay + target;
         } else if (previousTarget === "=") {
             btnDisplay = target;
+            calcDisplay = target;
             totalSum = 0;
             totalArray = [];
         } else {
             btnDisplay = btnDisplay + target;
+            calcDisplay = calcDisplay + target;
         }
 
     }
@@ -68,36 +83,56 @@ function showWhatIsPressed(event) {
         totalArray.push(btnDisplay);
         calculateTotal(totalArray);
         btnDisplay = totalSum;
+        calcDisplay = calcDisplay + target + totalSum;
         totalArray.push(totalSum);
     }
     pressedBtnDisplay.innerHTML = btnDisplay;
+    calculationDisplay.innerText = calcDisplay;
     // changes the value of the previous target variable  
     previousTarget = target;
 }
 
-// target for calculation display container
-const calculationDisplay = document.getElementById("calculation");
-// variable for showing calculation
-let calcDisplay = "";
 
 
-function showCalculation(event) {
-    let target = event.target.innerText;
+// function showCalculation(event) {
+//     console.log("1",previousTarget);
+//     let target = event.target.innerText;
 
-    if (target === "AC") {
-        calcDisplay = btnDisplay;
-    }
+//     if (target === "AC") {
+//         calcDisplay = "";
+//     }
 
-    if (symbolArray.includes(target)) {
-        if (previousTarget === "=" ) {
-            calcDisplay = calcDisplay + "=" + btnDisplay;
-        }
-    }
-    
+//     if (symbolArray.includes(target)) {
+//         if (previousTarget === "=") {
+//             calcDisplay = calcDisplay + target;
+//         }
+        
+//         if (symbolArray.includes(previousTarget)) {
+//             calcDisplay = calcDisplay.substr(0,calcDisplay.length-1);
+//         } 
+//             // calcDisplay = calcDisplay + target;
+//     }
 
-    calculationDisplay.innerHTML = calcDisplay;
+//     if (target === "." && btnDisplay.includes(".") === false) {
+//         calcDisplay = calcDisplay + target;
+//     }
 
-}
+//     if (target <=9 && target >=0) {
+//         if (previousTarget === "=") {
+//             calcDisplay = target;
+//         } else {
+//             calcDisplay = calcDisplay + target;
+//         }
+//     }
+
+//     if (target === "=" && previousTarget !== "=") {
+//         calcDisplay = calcDisplay + target + btnDisplay;
+//     }
+
+//     calculationDisplay.innerText = calcDisplay;
+//     console.log("2",previousTarget);
+
+// }
 
 // total sum
 let totalSum = 0;
@@ -169,5 +204,5 @@ function calculateTotal(array) {
 
     totalSum = array.join("");
     totalArray = [];
-    
+
 }
